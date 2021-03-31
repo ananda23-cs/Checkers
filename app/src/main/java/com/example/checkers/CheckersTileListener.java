ee82a419cf586534f4f7dbff2d66c4cac250b869
caitlinc <ching23@up.edu>
2021-03-31 11:52:12 -1000

    Implemented CheckersMainActivity and CheckersPiece and updated board in activity_main.xml

---
+++ app/src/main/java/com/example/checkers/CheckersTileListener.java
@@ -0,0 +1,10 @@
+package com.example.checkers;
+
+import android.view.View;
+
+public class CheckersTileListener implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
