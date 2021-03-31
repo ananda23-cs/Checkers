32df7de3e3edad10e709dc301ea1281af6fa9605
ananda23-cs <ananda23@up.edu>
2021-03-31 14:47:27 -0700

    integrated dimens.xml into the res folder of the game

--- app/src/main/java/com/example/checkers/CheckersMainActivity.java
+++ app/src/main/java/com/example/checkers/CheckersMainActivity.java
@@ -4,6 +4,7 @@ import com.example.checkers.game.GameFramework.GameMainActivity;
 import com.example.checkers.game.GameFramework.LocalGame;
 import com.example.checkers.game.GameFramework.gameConfiguration.GameConfig;
 import com.example.checkers.game.GameFramework.infoMessage.GameState;
+import com.example.checkers.game.GameFramework.utilities.Saving;
 
 public class CheckersMainActivity extends GameMainActivity {
 
@@ -28,5 +29,31 @@ public class CheckersMainActivity extends GameMainActivity {
      */
     public LocalGame createLocalGame(GameState gameState) {
         return null;
+    }
+
+    /**
+     * saveGame, adds this games prepend to the filename
+     *
+     * @param gameName
+     * 				Desired save name
+     * @return String representation of the save
+     */
+    @Override
+    public GameState saveGame(String gameName) {
+        return super.saveGame(getGameString(gameName));
+    }
+
+    /**
+     * loadGame, adds this games prepend to the desire file to open and creates the game specific state
+     * @param gameName
+     * 				The file to open
+     * @return The loaded GameState
+     */
+    @Override
+    public GameState loadGame(String gameName){
+        String appName = getGameString(gameName);
+        super.loadGame(appName);
+        return (GameState) new CheckersGameState((CheckersGameState) Saving.readFromFile(appName,
+                                                                    this.getApplicationContext()));
     }
 }
