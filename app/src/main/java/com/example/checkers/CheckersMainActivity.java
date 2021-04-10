36b0ee8960cf0bf71f4329edb735c5dbe1b900ea
ananda23-cs <ananda23@up.edu>
2021-04-10 09:20:47 -0700

    changed default config so that the computer player is dumb

--- app/src/main/java/com/example/checkers/CheckersMainActivity.java
+++ app/src/main/java/com/example/checkers/CheckersMainActivity.java
@@ -46,14 +46,14 @@ public class CheckersMainActivity extends GameMainActivity {
         });
 
         // dumb computer player
-        playerTypes.add(new GamePlayerType("Computer Player (smart)") {
+        playerTypes.add(new GamePlayerType("Computer Player (dumb)") {
             public GamePlayer createPlayer(String name) {
                 return new CheckersComputerPlayer1(name);
             }
         });
 
         // smarter computer player
-        playerTypes.add(new GamePlayerType("Computer Player (dumb)") {
+        playerTypes.add(new GamePlayerType("Computer Player (smart)") {
             public GamePlayer createPlayer(String name) {
                 return new CheckersComputerPlayer2(name);
             }
@@ -64,7 +64,7 @@ public class CheckersMainActivity extends GameMainActivity {
 
         // Add the default players
         defaultConfig.addPlayer("Human", 0); // player 1: a human player
-        defaultConfig.addPlayer("Computer", 2); // player 2: a computer player
+        defaultConfig.addPlayer("Computer", 1); // player 2: a computer player
 
         // Set the initial information for the remote players
         defaultConfig.setRemoteData("Remote Human Player", "", 0);
