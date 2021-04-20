a1922c8e0b642642fc7dbecc12ad029502dcd420
ananda23-cs <ananda23@up.edu>
2021-04-20 12:10:48 -0700

    added more copy constructor and equal game state tests in CheckersTest

--- app/src/test/java/com/example/checkers/CheckersTest.java
+++ app/src/test/java/com/example/checkers/CheckersTest.java
@@ -37,4 +37,31 @@ public class CheckersTest {
         CheckersGameState otherState = new CheckersGameState();
         assertTrue("The game states are not equal.", checkersGameState.equals(otherState));
     }
+
+    @Test
+    public void test_EqualsState_InProgress(){
+        CheckersGameState checkersState = new CheckersGameState();
+        checkersState.setPieceSelectedPieceAndPieceSelectedBoolean(2,2);
+        if(checkersState.canMove(checkersState.getPieceSelectedPiece(),1,1,
+                checkersState.getPlayerTurn())) {
+            checkersState.move(1, 1);
+        }
+        checkersState.setPieceSelectedPieceAndPieceSelectedBoolean(5,5);
+        if(checkersState.canMove(checkersState.getPieceSelectedPiece(),1,-1,
+                checkersState.getPlayerTurn())) {
+            checkersState.move(1, -1);
+        }
+        CheckersGameState otherState = new CheckersGameState();
+        otherState.setPieceSelectedPieceAndPieceSelectedBoolean(2,2);
+        if(otherState.canMove(otherState.getPieceSelectedPiece(),1,1,
+                otherState.getPlayerTurn())) {
+            otherState.move(1, 1);
+        }
+        otherState.setPieceSelectedPieceAndPieceSelectedBoolean(5,5);
+        if(otherState.canMove(otherState.getPieceSelectedPiece(),1,-1,
+                otherState.getPlayerTurn())) {
+            otherState.move(1, -1);
+        }
+        assertTrue("The game states are not equal.", checkersState.equals(otherState));
+    }
 }
