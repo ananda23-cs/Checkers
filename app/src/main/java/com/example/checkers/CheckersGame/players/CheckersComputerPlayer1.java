08583eaa6b9d8c6aa391612f1d831ffd2df67aba
ananda23-cs <ananda23@up.edu>
2021-04-21 23:44:22 -0700

    minor fix in computer player classes

--- app/src/main/java/com/example/checkers/CheckersGame/players/CheckersComputerPlayer1.java
+++ app/src/main/java/com/example/checkers/CheckersGame/players/CheckersComputerPlayer1.java
@@ -8,9 +8,6 @@
 
 package com.example.checkers.CheckersGame.players;
 
-import android.util.Log;
-
-import com.example.checkers.CheckersGame.Actions.CheckersCancelMoveAction;
 import com.example.checkers.CheckersGame.Actions.CheckersChoosePieceAction;
 import com.example.checkers.CheckersGame.infoMessage.CheckersGameState;
 import com.example.checkers.CheckersGame.infoMessage.CheckersPiece;
@@ -51,57 +48,51 @@ public class CheckersComputerPlayer1 extends GameComputerPlayer {
 
 
             sleep(1);
+            //list of possible moves
+            ArrayList<CheckersChoosePieceAction[]> possibleMoves = new ArrayList<CheckersChoosePieceAction[]>();
+            CheckersPiece [] Pieces;
 
-            if(((CheckersGameState) info).getPlayerTurn()==1) {
-                //list of possible moves
-                ArrayList<CheckersChoosePieceAction[]> possibleMoves = new ArrayList<CheckersChoosePieceAction[]>();
-                CheckersPiece [] Pieces;
+            if(((CheckersGameState) info).getPlayerTurn() == 1){
+                Pieces = ((CheckersGameState) info).p2Pieces;
+            }
+            else{
+                Pieces = ((CheckersGameState) info).p1Pieces;
+            }
 
-                if(playerNum == 1){
-                    Pieces = ((CheckersGameState) info).p2Pieces;
+
+            //runs through all the computer players pieces checks all the moves. It adds all possible moves to the
+            //arraylist above
+            for(CheckersPiece piece: Pieces){
+
+                //if moving left backwards is a possible move for this piece it gets added to the list of moves
+                checkValidMove(((CheckersGameState)info),piece,-1,-1,possibleMoves);
+                //if moving right backwards is a possible move for this piece it gets added to the list of moves
+                checkValidMove(((CheckersGameState)info),piece,+1,-1,possibleMoves);
+                //if moving right Forward is a possible move for this piece it gets added to the list of moves
+                checkValidMove(((CheckersGameState)info),piece,+1,+1,possibleMoves);
+                //if moving left Forward is a possible move for this piece it gets added to the list of moves
+                checkValidMove(((CheckersGameState)info),piece,-1,+1,possibleMoves);
+
+                //if capturing right backwards is a valid move it gets added to the list of possible moves
+                checkValidCapture(((CheckersGameState)info),piece,possibleMoves,+1,-1);
+                //if capturing left backwards is a valid move it gets added to the list of possible moves
+                checkValidCapture(((CheckersGameState)info),piece,possibleMoves,-1,-1);
+                //if capturing right forwards is a valid move it gets added to the list of possible moves
+                checkValidCapture(((CheckersGameState)info),piece,possibleMoves,+1,+1);
+                //if capturing left forwards is a valid move it gets added to the list of possible moves
+                checkValidCapture(((CheckersGameState)info),piece,possibleMoves,-1,+1);
+
+
+
+
+            }
+
+            int possibleMovesIndex = (int)(Math.random()*(possibleMoves.size()));
+            if(possibleMoves.size() >0) {
+                for (CheckersChoosePieceAction action : possibleMoves.get(possibleMovesIndex)) {
+                    game.sendAction(action);
+                    game.sendAction(action);
                 }
-                else{
-                    Pieces = ((CheckersGameState) info).p1Pieces;
-                }
-
-
-                //runs through all the computer players pieces checks all the moves. It adds all possible moves to the
-                //arraylist above
-                for(CheckersPiece piece: Pieces){
-
-                    //if moving left backwards is a possible move for this piece it gets added to the list of moves
-                    checkValidMove(((CheckersGameState)info),piece,-1,-1,possibleMoves);
-                    //if moving right backwards is a possible move for this piece it gets added to the list of moves
-                    checkValidMove(((CheckersGameState)info),piece,+1,-1,possibleMoves);
-                    //if moving right Forward is a possible move for this piece it gets added to the list of moves
-                    checkValidMove(((CheckersGameState)info),piece,+1,+1,possibleMoves);
-                    //if moving left Forward is a possible move for this piece it gets added to the list of moves
-                    checkValidMove(((CheckersGameState)info),piece,-1,+1,possibleMoves);
-
-                    //if capturing right backwards is a valid move it gets added to the list of possible moves
-                    checkValidCapture(((CheckersGameState)info),piece,possibleMoves,+1,-1);
-                    //if capturing left backwards is a valid move it gets added to the list of possible moves
-                    checkValidCapture(((CheckersGameState)info),piece,possibleMoves,-1,-1);
-                    //if capturing right forwards is a valid move it gets added to the list of possible moves
-                    checkValidCapture(((CheckersGameState)info),piece,possibleMoves,+1,+1);
-                    //if capturing left forwards is a valid move it gets added to the list of possible moves
-                    checkValidCapture(((CheckersGameState)info),piece,possibleMoves,-1,+1);
-
-
-
-
-                }
-
-                int possibleMovesIndex = (int)(Math.random()*(possibleMoves.size()));
-                if(possibleMoves.size() >0) {
-                    for (CheckersChoosePieceAction action : possibleMoves.get(possibleMovesIndex)) {
-                        game.sendAction(action);
-                        game.sendAction(action);
-                    }
-                }
-
-
-
             }
         }
     } //receiveInfo
